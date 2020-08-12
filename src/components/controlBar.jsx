9e1d7de36dbecd708a26d8936d62d51d473e5c21
Logan Sorensen <loganmsorensen@gmail.com>
2020-08-12 06:02:07 -0700

    added more styling to the controls component

--- src/components/controlBar.jsx
+++ src/components/controlBar.jsx
@@ -2,16 +2,37 @@ import React from "react";
 
 const ControlBar = () => {
   return (
-  <div className="controls-bar">
-    <div className="song-info">
-      <div className="names">
-      <a className="song-name">Placeholder Name</a>
-      <a className="artist-name">Placeholder Name</a>
+    <div className="controls-bar">
+      <div className="song-info">
+        <div className="name-and-heart">
+          <a href="#" className="song-name">Are You What You Want To Be?</a>
+        <i class="far fa-heart tooltip"></i>
+        </div>
+          <a href="#" className="artist-name">Foster The People</a>
       </div>
-      <i class="far fa-heart"></i>
-    </div>
-      <div className="controls"></div>
-      <div className="queue-and-audio"></div>
+      <div className="controls">
+        <div className="buttons">
+        <i class="fas fa-random"></i>
+        <i class="fas fa-step-backward"></i>
+        <i class="far fa-play-circle"></i>
+        <i class="fas fa-step-forward"></i>
+        <i class="fas fa-redo-alt"></i>
+        </div>
+      </div>
+      <div className="queue-and-audio">
+        <i class="fas fa-indent tooltip">
+          <span className="tooltiptext">Queue</span>
+        </i>
+        <i class="fas fa-laptop-house tooltip">
+          <span className="tooltiptext">Devices Available</span>
+        </i>
+        <div className="volume">
+          <i class="fas fa-volume-up tooltip">
+            <span className="tooltiptext">Mute</span>
+          </i>
+          <input type="range" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/libraryNav.jsx
+++ src/components/libraryNav.jsx
@@ -5,12 +5,12 @@ const LibraryNav = () => {
     <div className="lib-and-playlist-links">
       <div className="lib-links">
         <h3 className="library-header">YOUR LIBRARY</h3>
-        <a href="">Made For You</a>
-        <a href="">Recently Played</a>
-        <a href="">Liked Songs</a>
-        <a href="">Albums</a>
-        <a href="">Artists</a>
-        <a href="">Podcasts</a>
+        <a href="#">Made For You</a>
+        <a href="#">Recently Played</a>
+        <a href="#">Liked Songs</a>
+        <a href="#">Albums</a>
+        <a href="#">Artists</a>
+        <a href="#">Podcasts</a>
       </div>
       <div className="playlist-links">
         <h3 className="playlist-header">PLAYLISTS</h3>
